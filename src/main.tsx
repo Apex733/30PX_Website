578b3a7f3f36c376ee06cea649b5c9cf13bfc89b
Ali Raza <aliraza@Alis-MacBook-Air.local>
2026-02-22 23:18:15 +0500

    Auto-sync: 2026-02-22 23:18:15

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import "@fontsource/inter/400.css";
-import "@fontsource/inter/500.css";
-import "@fontsource/inter/600.css";
-import "@fontsource/inter/700.css";
 import { HashRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import Lenis from 'lenis';
